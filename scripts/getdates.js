cf16b0ec226267104b14583ebdce7caecaab41f7
dbunker89 <dylanbee1989@gmail.com>
2024-06-15 15:33:47 -0700

    week 1

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,11 +1,4 @@
-// Get the current year
-const currentYear = new Date().getFullYear();
-
-// Set the current year in the first paragraph of the footer
-document.getElementById('copyright-year').textContent = `© ${currentYear}`;
-
-// Get the last modified date of the document
-const lastModifiedDate = document.lastModified;
-
-// Set the last modified date in the second paragraph of the footer
-document.getElementById('last-modified').textContent = `Last Modified: ${lastModifiedDate}`;
+document.addEventListener('DOMContentLoaded', (event) => {
+    const lastModified = document.lastModified;
+    document.getElementById("date-time").textContent = lastModified;
+});
